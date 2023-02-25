c9b999a35e8f87265f2df05c1faa3a1fb32d0b4e
Laye <tlayefaah@gmail.com>
2023-02-25 10:49:38 +0100

    login authentication

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
 import { Store } from '@/utils/store';
 import Head from 'next/head';
 import Link from 'next/link';
+import { ToastContainer } from 'react-toastify';
 import React, { useContext, useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Layout({ title, children }) {
+  const { status, data: session } = useSession();
+
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
   const [CartItemsCount, setCartItemsCount] = useState(0);
@@ -24,6 +29,8 @@ export default function Layout({ title, children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <ToastContainer position="bottom-center" limit={1} />
       <div className="flex min-h-screen flex-col justify-between">
         <header>
           <nav className="flex h-12 justify-between shadow-md items-center px-4">
@@ -41,9 +48,15 @@ export default function Layout({ title, children }) {
                   )}
                 </a>
               </Link>
-              <Link legacyBehavior href="/login">
-                <a className="p-2">Login</a>
-              </Link>
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link legacyBehavior href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+              )}
             </div>
           </nav>
         </header>
